97b156c42a9ab0e5b3567429bd97340d2ef2a6ac
Haeryz <haeriz42069@gmail.com>
2025-03-30 13:47:50 +0700

    feat: implement Sidebar, Separator, Sheet, Skeleton, and Tooltip components with responsive design and enhanced functionality

--- frontend/src/components/ui/separator.tsx
+++ frontend/src/components/ui/separator.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
 import { cn } from "@/lib/utils"
 
 function Separator({
@@ -6,18 +10,14 @@ function Separator({
   orientation = "horizontal",
   decorative = true,
   ...props
-}: React.HTMLAttributes<HTMLDivElement> & {
-  orientation?: "horizontal" | "vertical"
-  decorative?: boolean
-}) {
+}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
   return (
-    <div
-      data-slot="separator"
-      role={decorative ? "none" : "separator"}
-      aria-orientation={decorative ? undefined : orientation}
+    <SeparatorPrimitive.Root
+      data-slot="separator-root"
+      decorative={decorative}
+      orientation={orientation}
       className={cn(
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
         className
       )}
       {...props}
